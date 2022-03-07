aff0b80f206f7b5843ab9c3204bfd13d4f07994b
AntoineDuplaa <63778482+AntoineDuplaa@users.noreply.github.com>
2022-03-07 12:21:04 +0100

    [FIX] removing background color page shop

--- lib/pages/shop/components/body.dart
+++ lib/pages/shop/components/body.dart
@@ -13,16 +13,6 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
             child: SingleChildScrollView(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      top: BorderSide(width: 1.0, color: Color(0xFFDFDFDF)),
-                      left: BorderSide(width: 1.0, color: Color(0xFFDFDFDF)),
-                      right: BorderSide(width: 1.0, color: Color(0xFF7F7F7F)),
-                      bottom: BorderSide(width: 1.0, color: Color(0xFF7F7F7F)),
-                    ),
-                    color: Color(0xFFBFBFBF),
-                  ),
                   child: Column(
                     children: [
                       Wrap(
@@ -43,7 +33,6 @@ class Body extends StatelessWidget {
                     ],
                   ),
                 ),
-              ),
-            );
+              );
   }
 }
